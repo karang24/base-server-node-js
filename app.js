0b136b12609ad5b88b9b637f16651c5732ef45fa
karang24 <tunggal.jaka@gmail.com>
2017-02-09 11:58:50 +0700

    jaka

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var http = require("http");
 
 http.createServer(function(req,res){
+	if(req.url !="/favicon.ico"){
 	res.writeHead(200,{"Content-Type": "text/plain"});
-	res.write("hello from base server no js");
+	res.write("hello from base server node js\n");
+	res.write("you req :"+req.url);
 	res.end();
+	
+	}
 	
 }).listen(8080);//localhost server 8080
 
